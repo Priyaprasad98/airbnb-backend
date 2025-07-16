58e904f8617486b2e065ffbe580dd9eb84ca95c5
Priya98 <priyakumari.prasad15@gmail.com>
2025-07-16 19:19:26 +0530

    improve ui of 404

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,3 @@
 exports.get404 = (req,res,next) => {
   res.status(404).render('404page' , {pageTitle: "page not found",currentPage: "404page" });
 }
-
-exports.getAddHome = (req,res,next) => {
-  res.render('host/homeAdd' , {pageTitle: "airbnb Add Home", currentPage: 'homeAdd'});
-}

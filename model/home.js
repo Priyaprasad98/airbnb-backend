4a144d7ec384c333ab6ca0833d27d8aa2d3745fc
Priya98 <priyakumari.prasad15@gmail.com>
2025-07-25 19:55:19 +0530

    build homeSchema

--- model/home.js
+++ model/home.js
@@ -1,61 +1,29 @@
-//local model
+const mongoose = require("mongoose");
 
-
-module.exports = class Home {
-
-  constructor(houseName, price, location, rating, img, description, _id) {
-     this.houseName = houseName;
-     this.price = price;
-     this.location = location;
-     this.rating = rating;
-     this.img = img;
-     this.description = description;
-     if(_id) {
-      this._id = _id; //if new home will be added it will be undefined and if edit home will be done that it will have id
-     }
-      
+const homeSchema = new mongoose.Schema({
+  houseName: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+  },
+  description: {
+    type: String,
   }
-  
-  save() {
-    const db = getDB();
-    if(this._id) { //update case
-      const updateFields = {
-        houseName: this.houseName,
-        price: this.price,
-        location: this.location,
-        img: this.img,
-        description: this.description
-      }
-      return db
-        .collection("homes")
-        .updateOne(
-          { _id: new ObjectId(String(this._id)) },
-          { $set: updateFields}
-        );
-    } else { //insert case
-       return db.collection("homes").insertOne(this).then((result)=>{
-       console.log(result);
-      });
-    }  
-  }
-  
-  static fetchAll() {
-    const db = getDB();
-    return db.collection("homes").find().toArray(); //this promise returns cursor that we need to convert in array 
-  }
+});
 
-  static findById(homeId) {
-    console.log(homeId);
-   const db = getDB();
-    return db.collection("homes")
-    .find({_id: new ObjectId(String(homeId))})
-    .next();
-  }
+module.exports = mongoose.model('Home', homeSchema);
 
-  static removeHome(homeId) {
-    const db = getDB();
-    return db.collection("homes")
-    .deleteOne({_id: new ObjectId(String(homeId))});
-  }
-
-} 

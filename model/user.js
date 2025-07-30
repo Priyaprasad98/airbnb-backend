fbef19e9a44cf0503468f92a8bab59e619a8da39
Priya98 <priyakumari.prasad15@gmail.com>
2025-07-30 19:08:47 +0530

    created user model to collect user's informations from signup

---
+++ model/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userType: {
+    type: String,
+    enum: ["guest", "host"],
+    default: guest
+  }
+
+});
+
+
+module.exports = mongoose.model('User', userSchema);
+

7cc7c009a81feaa032b06dc093ee3c5c24f19abd
Priya98 <priyakumari.prasad15@gmail.com>
2025-08-01 17:18:42 +0530

    Add 403 Access Denied page and controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,16 @@
-exports.get404 = (req,res,next) => {
+exports.get404 = (err,req,res,next) => {
   res.status(404).render('404page',{
     pageTitle: "page not found",
     currentPage: "404page",
+    isLoggedIn: req.isLoggedIn,
+    user: req.session.user 
+  });
+}
+
+exports.get403 = (req,res,next) => {
+  res.status(403).render('403page', {
+    pageTitle: "Access Denied",
+    currentPage: "403page",
     isLoggedIn: req.isLoggedIn,
     user: req.session.user 
   });
